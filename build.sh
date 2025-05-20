c455ffc0d19624b7a904c32dca9102b5fe8156b9
Watson Dinh <mr.kurro@gmail.com>
2025-05-20 17:12:35 -0500

    update build script

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 
 IMAGE_NAME="chickentooth/cs544-quizz"
-IMAGE_TAG="latest"
+COMMIT_ID=$(git rev-parse --short HEAD)
+FULL_IMAGE_NAME_WITH_TAG="${IMAGE_NAME}:${COMMIT_ID}"
 
-echo "Building Docker image ${IMAGE_NAME}:${IMAGE_TAG}..."
-docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
+echo "Building Docker image ${FULL_IMAGE_NAME_WITH_TAG}..."
+docker build -t "${FULL_IMAGE_NAME_WITH_TAG}" .
 
-echo "Docker image build complete: ${IMAGE_NAME}:${IMAGE_TAG}"
+echo "Docker image build complete: ${FULL_IMAGE_NAME_WITH_TAG}"
+
+echo "Pushing Docker image ${FULL_IMAGE_NAME_WITH_TAG}..."
+docker push "${FULL_IMAGE_NAME_WITH_TAG}"
+echo "Docker image push complete: ${FULL_IMAGE_NAME_WITH_TAG}"
 

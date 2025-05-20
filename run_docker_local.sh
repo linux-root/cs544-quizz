9b9e70b4a97448ebcb9e49c0ade5bf0f614c10dc
Watson Dinh <mr.kurro@gmail.com>
2025-05-20 13:05:27 -0500

    change local run script

--- run_docker_local.sh
+++ run_docker_local.sh
@@ -1,4 +1,4 @@
-docker run -p 8081:8080 \
+docker run -p 5555:8080 \
   -e SPRING_DATASOURCE_URL=jdbc:postgresql://host.docker.internal:6432/cool_project_db \
   -e SPRING_DATASOURCE_USERNAME=postgres \
   -e SPRING_DATASOURCE_PASSWORD=s3cret! \

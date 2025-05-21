66a0b609002ae36820f758877d525cccec1bc553
Watson Dinh <mr.kurro@gmail.com>
2025-05-21 13:18:25 -0500

    clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,5 @@
 module.exports = {
   content: [
     "./src/main/resources/templates/**/*.html",
-    // "./src/main/resources/static/**/*.js" // Keep this commented for now or add it back if you use JS to add classes
-  ],
-  theme: {
-    extend: {},
-  },
-  // DaisyUI plugin and config removed for Tailwind CSS v4
-  // It will be configured in input.css
+  ]
 } 
